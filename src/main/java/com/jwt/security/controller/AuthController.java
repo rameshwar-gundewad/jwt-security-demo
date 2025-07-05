e04e2c2258250b375e01701eea8439751225372f
Rameshwar <gundewad.rameshwar.b@gmail.com>
2025-07-05 19:57:33 +0530

    Added the login controller

--- src/main/java/com/jwt/security/controller/AuthController.java
+++ src/main/java/com/jwt/security/controller/AuthController.java
@@ -1,9 +1,14 @@
 package com.jwt.security.controller;
 
+import com.jwt.security.model.UserRequest;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AuthController {
 
-
+        public String login(@RequestBody UserRequest userRequest) {
+            System.out.println("Got the user details to login!!");
+            return "Successfully generated the JWT Token!!";
+        }
 }
